c2cb39617530d3ab116a72240f194fd14bd8158a
12f23eddde <12f23eddde@gmail.com>
2023-01-21 13:16:08 +0800

    fix(frontend): react error 130
    several cjs react libs (e.g. react-reveal) confuse rollup, leading to a production-only bug: minified react error 130
    for more: https://github.com/vitejs/vite/issues/2139
    hope vite can fix this 1-yr-old bug asap

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import { version } from './package.json';
 
 process.env.REACT_APP_VERSION = version;
 
+// Without it dynamic require is not possible in config file
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 export default defineConfig(({command, mode}) => {
     let env = loadEnv(mode, process.cwd(), 'REACT_APP_');
     const processEnv = Object.fromEntries(Object.entries(process.env).filter(([key]) => key.startsWith('REACT_APP_')));
@@ -22,8 +26,19 @@ export default defineConfig(({command, mode}) => {
             splitVendorChunkPlugin(),
         ],
         envPrefix: 'REACT_APP_',
+        // walkaround for rollup commonjs plugin
+        // credit: https://github.com/vitejs/vite/issues/2139#issuecomment-1399098579
         build: {
             outDir: 'build',
+            commonjsOptions: {
+                defaultIsModuleExports(id) {
+                  const module = require(id);
+                  if (module?.default) {
+                    return false;
+                  }
+                  return "auto";
+                },
+              },
         },
         define: {
             __APP_ENV__: env.APP_ENV,
